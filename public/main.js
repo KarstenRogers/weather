d4ce554d249b9c660edb3b4c3d8665ea3bcdf679
Karsten Rogers <karstenrogers@Karstens-Air.localdomain>
2018-09-20 09:19:08 -0400

    weather

--- public/main.js
+++ public/main.js
@@ -1,10 +1,13 @@
 const clickedOnCity = () => {
   let searchInput = document.querySelector('.city-input')
   let citySearch = searchInput.value
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=59fa88271712f1716837d764589f204a`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+`${citySearch}&units=imperial&appid=59fa88271712f1716837d764589f204a`)
 .then(response => {
   return response.json()
-}).then(weatherAttributes => {
+})
+
+
+.then(weatherAttributes => {
       let weatherOnPage = document.querySelector('.weather-display')
 
       let weatherLiName = document.createElement('li')
@@ -27,21 +30,31 @@ const clickedOnCity = () => {
 
 }
 
+class weatherList {
+  constructor(parentSelector) {
+    this.parent = document.querySelector(parentSelector)
+  }
 
+  addCondition(parent, className, word) {
+    let newLi = document.createElement('li')
+    newLi.classList.add('conditions')
+    newLi.textContent = 'the ' + className + ' is ' + word
+    parent.appendChild(newLi)
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class weatherAPI {
+  constructor(json) {
+    this.json = json
+  }
+  weatherCondition(json) {
+    let WeatherList = new weatherList('.weatherconditions')
+    WeatherList.addCondition(parent, 'humidity', json.main.humidity)
+    WeatherList.addCondition(parent, 'temp', json.main.temp)
+    WeatherList.addCondition(parent, 'high', json.main.temp_max)
+    WeatherList.addCondition(parent, 'low', json.main.temp_min)
+  }
+}
 const main = () => {
 
 let city = document.querySelector('.city')
